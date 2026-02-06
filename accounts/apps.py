a5140bf09ad14d6bcc95f09ecf0c398b482f1aa9
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2026-02-06 20:45:12 +0530

    Fix admin auto-create using AppConfig

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
-
+import os
 
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        if os.environ.get("AUTO_CREATE_ADMIN") == "True":
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+
+            username = os.environ.get("ADMIN_USERNAME", "admin")
+            email = os.environ.get("ADMIN_EMAIL", "admin@example.com")
+            password = os.environ.get("ADMIN_PASSWORD", "admin123")
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username, email, password)
--- healthcare_system/settings.py
+++ healthcare_system/settings.py
@@ -174,13 +174,3 @@ X_FRAME_OPTIONS = 'DENY'
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-from django.contrib.auth import get_user_model
-
-if os.environ.get("AUTO_CREATE_ADMIN") == "True":
-    User = get_user_model()
-    username = os.environ.get("ADMIN_USERNAME", "admin")
-    email = os.environ.get("ADMIN_EMAIL", "admin@example.com")
-    password = os.environ.get("ADMIN_PASSWORD", "admin123")
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
